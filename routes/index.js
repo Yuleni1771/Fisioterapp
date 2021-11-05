5cfd5f1c61bc36275b39a65fb467fadc4daf7db1
OzPineapple <zush18@outlook.com>
2021-11-04 22:22:51 -0600

    Eliminar código no usado de prueba

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,10 @@
 var express	= require('express');
-var mysql	= require('mysql');
 var db		= require('../lib/mysql.js');
 var router	= express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
-});
-
-router.post('/login', async function (req, res, next) {
-	console.table( req.body );
-	try{
-		await db.loginFisio( req.body );
-		res.status(204).send();
-	}catch( err ){
-		if( err.status == 404 )
-			res.status( 404 ).send();
-		else
-			res.status( 500 ).send();
-	}
 });
 
 router.get("/fisios", async (req, res) => {
